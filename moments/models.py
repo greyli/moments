4ebbaba5ec2e35ee72e07edc3f93d0cab3a3afca
Grey Li <withlihui@gmail.com>
2024-02-12 21:03:52 +0800

    Rewrite models to use SQLAlchemy API

--- moments/models.py
+++ moments/models.py
@@ -5,27 +5,29 @@ from flask import current_app
 from flask_avatars import Identicon
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
+from sqlalchemy import Column, Integer, String, Text, ForeignKey, Boolean, DateTime, event
+from sqlalchemy.orm import relationship
 
 from moments.core.extensions import db, whooshee
 
 # relationship table
 roles_permissions = db.Table('roles_permissions',
-                             db.Column('role_id', db.Integer, db.ForeignKey('role.id')),
-                             db.Column('permission_id', db.Integer, db.ForeignKey('permission.id'))
+                             Column('role_id', Integer, ForeignKey('role.id')),
+                             Column('permission_id', Integer, ForeignKey('permission.id'))
                              )
 
 
 class Permission(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(30), unique=True)
-    roles = db.relationship('Role', secondary=roles_permissions, back_populates='permissions')
+    id = Column(Integer, primary_key=True)
+    name = Column(String(30), unique=True)
+    roles = relationship('Role', secondary=roles_permissions, back_populates='permissions')
 
 
 class Role(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(30), unique=True)
-    users = db.relationship('User', back_populates='role')
-    permissions = db.relationship('Permission', secondary=roles_permissions, back_populates='roles')
+    id = Column(Integer, primary_key=True)
+    name = Column(String(30), unique=True)
+    users = relationship('User', back_populates='role')
+    permissions = relationship('Permission', secondary=roles_permissions, back_populates='roles')
 
     @staticmethod
     def init_role():
@@ -53,63 +55,63 @@ class Role(db.Model):
 
 # relationship object
 class Follow(db.Model):
-    follower_id = db.Column(db.Integer, db.ForeignKey('user.id'),
+    follower_id = Column(Integer, ForeignKey('user.id'),
                             primary_key=True)
-    followed_id = db.Column(db.Integer, db.ForeignKey('user.id'),
+    followed_id = Column(Integer, ForeignKey('user.id'),
                             primary_key=True)
-    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+    timestamp = Column(DateTime, default=datetime.utcnow)
 
-    follower = db.relationship('User', foreign_keys=[follower_id], back_populates='following', lazy='joined')
-    followed = db.relationship('User', foreign_keys=[followed_id], back_populates='followers', lazy='joined')
+    follower = relationship('User', foreign_keys=[follower_id], back_populates='following', lazy='joined')
+    followed = relationship('User', foreign_keys=[followed_id], back_populates='followers', lazy='joined')
 
 
 # relationship object
 class Collect(db.Model):
-    collector_id = db.Column(db.Integer, db.ForeignKey('user.id'),
+    collector_id = Column(Integer, ForeignKey('user.id'),
                              primary_key=True)
-    collected_id = db.Column(db.Integer, db.ForeignKey('photo.id'),
+    collected_id = Column(Integer, ForeignKey('photo.id'),
                              primary_key=True)
-    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+    timestamp = Column(DateTime, default=datetime.utcnow)
 
-    collector = db.relationship('User', back_populates='collections', lazy='joined')
-    collected = db.relationship('Photo', back_populates='collectors', lazy='joined')
+    collector = relationship('User', back_populates='collections', lazy='joined')
+    collected = relationship('Photo', back_populates='collectors', lazy='joined')
 
 
 @whooshee.register_model('name', 'username')
 class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, index=True)
-    email = db.Column(db.String(254), unique=True, index=True)
-    password_hash = db.Column(db.String(128))
-    name = db.Column(db.String(30))
-    website = db.Column(db.String(255))
-    bio = db.Column(db.String(120))
-    location = db.Column(db.String(50))
-    member_since = db.Column(db.DateTime, default=datetime.utcnow)
-    avatar_s = db.Column(db.String(64))
-    avatar_m = db.Column(db.String(64))
-    avatar_l = db.Column(db.String(64))
-    avatar_raw = db.Column(db.String(64))
+    id = Column(Integer, primary_key=True)
+    username = Column(String(20), unique=True, index=True)
+    email = Column(String(254), unique=True, index=True)
+    password_hash = Column(String(128))
+    name = Column(String(30))
+    website = Column(String(255))
+    bio = Column(String(120))
+    location = Column(String(50))
+    member_since = Column(DateTime, default=datetime.utcnow)
+    avatar_s = Column(String(64))
+    avatar_m = Column(String(64))
+    avatar_l = Column(String(64))
+    avatar_raw = Column(String(64))
 
-    confirmed = db.Column(db.Boolean, default=False)
-    locked = db.Column(db.Boolean, default=False)
-    active = db.Column(db.Boolean, default=True)
+    confirmed = Column(Boolean, default=False)
+    locked = Column(Boolean, default=False)
+    active = Column(Boolean, default=True)
 
-    public_collections = db.Column(db.Boolean, default=True)
-    receive_comment_notification = db.Column(db.Boolean, default=True)
-    receive_follow_notification = db.Column(db.Boolean, default=True)
-    receive_collect_notification = db.Column(db.Boolean, default=True)
+    public_collections = Column(Boolean, default=True)
+    receive_comment_notification = Column(Boolean, default=True)
+    receive_follow_notification = Column(Boolean, default=True)
+    receive_collect_notification = Column(Boolean, default=True)
 
-    role_id = db.Column(db.Integer, db.ForeignKey('role.id'))
+    role_id = Column(Integer, ForeignKey('role.id'))
 
-    role = db.relationship('Role', back_populates='users')
-    photos = db.relationship('Photo', back_populates='author', cascade='all')
-    comments = db.relationship('Comment', back_populates='author', cascade='all')
-    notifications = db.relationship('Notification', back_populates='receiver', cascade='all')
-    collections = db.relationship('Collect', back_populates='collector', cascade='all')
-    following = db.relationship('Follow', foreign_keys=[Follow.follower_id], back_populates='follower',
+    role = relationship('Role', back_populates='users')
+    photos = relationship('Photo', back_populates='author', cascade='all')
+    comments = relationship('Comment', back_populates='author', cascade='all')
+    notifications = relationship('Notification', back_populates='receiver', cascade='all')
+    collections = relationship('Collect', back_populates='collector', cascade='all')
+    following = relationship('Follow', foreign_keys=[Follow.follower_id], back_populates='follower',
                                 lazy='dynamic', cascade='all')
-    followers = db.relationship('Follow', foreign_keys=[Follow.followed_id], back_populates='followed',
+    followers = relationship('Follow', foreign_keys=[Follow.followed_id], back_populates='followed',
                                 lazy='dynamic', cascade='all')
 
     def __init__(self, **kwargs):
@@ -211,65 +213,65 @@ class User(db.Model, UserMixin):
 
 
 tagging = db.Table('tagging',
-                   db.Column('photo_id', db.Integer, db.ForeignKey('photo.id')),
-                   db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'))
+                   Column('photo_id', Integer, ForeignKey('photo.id')),
+                   Column('tag_id', Integer, ForeignKey('tag.id'))
                    )
 
 
 @whooshee.register_model('description')
 class Photo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    description = db.Column(db.String(500))
-    filename = db.Column(db.String(64))
-    filename_s = db.Column(db.String(64))
-    filename_m = db.Column(db.String(64))
-    timestamp = db.Column(db.DateTime, default=datetime.utcnow, index=True)
-    can_comment = db.Column(db.Boolean, default=True)
-    flag = db.Column(db.Integer, default=0)
-    author_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    id = Column(Integer, primary_key=True)
+    description = Column(String(500))
+    filename = Column(String(64))
+    filename_s = Column(String(64))
+    filename_m = Column(String(64))
+    timestamp = Column(DateTime, default=datetime.utcnow, index=True)
+    can_comment = Column(Boolean, default=True)
+    flag = Column(Integer, default=0)
+    author_id = Column(Integer, ForeignKey('user.id'))
 
-    author = db.relationship('User', back_populates='photos')
-    comments = db.relationship('Comment', back_populates='photo', cascade='all')
-    collectors = db.relationship('Collect', back_populates='collected', cascade='all')
-    tags = db.relationship('Tag', secondary=tagging, back_populates='photos')
+    author = relationship('User', back_populates='photos')
+    comments = relationship('Comment', back_populates='photo', cascade='all')
+    collectors = relationship('Collect', back_populates='collected', cascade='all')
+    tags = relationship('Tag', secondary=tagging, back_populates='photos')
 
 
 @whooshee.register_model('name')
 class Tag(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), index=True, unique=True)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(64), index=True, unique=True)
 
-    photos = db.relationship('Photo', secondary=tagging, back_populates='tags')
+    photos = relationship('Photo', secondary=tagging, back_populates='tags')
 
 
 class Comment(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    body = db.Column(db.Text)
-    timestamp = db.Column(db.DateTime, default=datetime.utcnow, index=True)
-    flag = db.Column(db.Integer, default=0)
+    id = Column(Integer, primary_key=True)
+    body = Column(Text)
+    timestamp = Column(DateTime, default=datetime.utcnow, index=True)
+    flag = Column(Integer, default=0)
 
-    replied_id = db.Column(db.Integer, db.ForeignKey('comment.id'))
-    author_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    photo_id = db.Column(db.Integer, db.ForeignKey('photo.id'))
+    replied_id = Column(Integer, ForeignKey('comment.id'))
+    author_id = Column(Integer, ForeignKey('user.id'))
+    photo_id = Column(Integer, ForeignKey('photo.id'))
 
-    photo = db.relationship('Photo', back_populates='comments')
-    author = db.relationship('User', back_populates='comments')
-    replies = db.relationship('Comment', back_populates='replied', cascade='all')
-    replied = db.relationship('Comment', back_populates='replies', remote_side=[id])
+    photo = relationship('Photo', back_populates='comments')
+    author = relationship('User', back_populates='comments')
+    replies = relationship('Comment', back_populates='replied', cascade='all')
+    replied = relationship('Comment', back_populates='replies', remote_side=[id])
 
 
 class Notification(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    message = db.Column(db.Text, nullable=False)
-    is_read = db.Column(db.Boolean, default=False)
-    timestamp = db.Column(db.DateTime, default=datetime.utcnow, index=True)
+    id = Column(Integer, primary_key=True)
+    message = Column(Text, nullable=False)
+    is_read = Column(Boolean, default=False)
+    timestamp = Column(DateTime, default=datetime.utcnow, index=True)
 
-    receiver_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    receiver_id = Column(Integer, ForeignKey('user.id'))
 
-    receiver = db.relationship('User', back_populates='notifications')
+    receiver = relationship('User', back_populates='notifications')
 
 
-@db.event.listens_for(User, 'after_delete', named=True)
+@event.listens_for(User, 'after_delete', named=True)
 def delete_avatars(**kwargs):
     target = kwargs['target']
     for filename in [target.avatar_s, target.avatar_m, target.avatar_l, target.avatar_raw]:
@@ -279,7 +281,7 @@ def delete_avatars(**kwargs):
                 os.remove(path)
 
 
-@db.event.listens_for(Photo, 'after_delete', named=True)
+@event.listens_for(Photo, 'after_delete', named=True)
 def delete_photos(**kwargs):
     target = kwargs['target']
     for filename in [target.filename, target.filename_s, target.filename_m]:
